3899ac778fc3f30cab7b0f70f778006f124656cc
Juho <s6heju04@gmail.com>
2020-01-31 09:57:27 +0200

    Updates

---
+++ precache-manifest.21c1bd500a33df0e8e908a119f91d85d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "464707214fe9d616360b576406ed818b",
+    "url": "/image-checker/static/media/brain.46470721.jpg"
+  },
+  {
+    "revision": "ed1c8c74c9c69ab97b4f",
+    "url": "/image-checker/static/js/runtime~main.ed1c8c74.js"
+  },
+  {
+    "revision": "77fa18547cd973535c39",
+    "url": "/image-checker/static/js/main.77fa1854.chunk.js"
+  },
+  {
+    "revision": "526f9b58d57151523a4e",
+    "url": "/image-checker/static/js/2.526f9b58.chunk.js"
+  },
+  {
+    "revision": "77fa18547cd973535c39",
+    "url": "/image-checker/static/css/main.aa98bfc2.chunk.css"
+  },
+  {
+    "revision": "526f9b58d57151523a4e",
+    "url": "/image-checker/static/css/2.f0b88e88.chunk.css"
+  },
+  {
+    "revision": "924a79634c0f7ff666943fb9191a0861",
+    "url": "/image-checker/index.html"
+  }
+];
